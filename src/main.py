010a6746982766140f08f0edc5fc118701abaac5
Simon Schellaert <me@simonschellaert.com>
2018-11-21 20:51:35 +0100

    Add .gitignore

--- src/main.py
+++ src/main.py
@@ -1,137 +1,113 @@
-# TODO: Integrate tensorboard
+import argparse
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim import lr_scheduler
 from torch.utils import data
-import numpy as np
-import torchvision
 from torchvision import datasets, models, transforms
-import matplotlib.pyplot as plt
-import time
-import os
-import copy
+
+from tensorboardX import SummaryWriter
 
 from data import TransformedDataset, train_test_split_dataset
 
 
-
-# TODO: Don't hardcode this
-num_classes = 11
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-
-
-full_dataset = datasets.ImageFolder('../data/train')
-train_dataset_original, val_dataset_original = train_test_split_dataset(full_dataset)
-
-train_dataset = TransformedDataset(train_dataset_original, transforms.Compose([
-    transforms.RandomResizedCrop(224, scale=(0.4, 1)),  # TODO: Fix me
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-]))
-
-val_dataset = TransformedDataset(val_dataset_original, transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-]))
+# TODO: Create a class Logger() that also writes model parameters at the beginning
+def write_loss(writer, epoch, loss, train):
+    label = 'Train' if train else 'Val'
+    writer.add_scalar(label + '/Loss', loss, epoch)
+    print('Epoch {} - {} Loss: {:.4f}'.format(epoch, label, loss))
 
 
+def train(model, device, dataloader, optimizer):
+    model.train()
+    average_loss = 0
 
-train_dataloader = data.DataLoader(train_dataset, batch_size=24, shuffle=True)
-val_dataloader = data.DataLoader(val_dataset, batch_size=24, shuffle=False)
+    for i, (inputs, labels) in enumerate(dataloader):
+        if i % 10 == 0:
+            print('Train epoch {} - {}/{}', epoch, i, len(dataloader))
 
-dataloaders = {
-    'train':  train_dataloader,
-    'val': val_dataloader
-}
+        optimizer.zero_grad()
+        outputs = model(inputs.to(device))
 
-dataset_sizes = {
-    'train': len(train_dataset),
-    'val': len(val_dataset)
-}
+        loss = criterion(outputs, labels.to(device))
+        loss.backward()
 
+        average_loss += loss.item() * inputs.size(0)
 
-def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
-    since = time.time()
+        optimizer.step()
 
-    best_model_wts = copy.deepcopy(model.state_dict())
-    best_loss = float('inf')
-
-    for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-        print('-' * 10)
-
-        # Each epoch has a training and validation phase
-        for phase in ['train', 'val']:
-            if phase == 'train':
-                scheduler.step()
-                model.train()  # Set model to training mode
-            else:
-                model.eval()   # Set model to evaluate mode
-
-            running_loss = 0.0
-
-            # Iterate over data.
-            for i, (inputs, labels) in enumerate(dataloaders[phase]):
-                if i % 10 == 0: print(i, '/', len(dataloaders[phase]))
-                inputs = inputs.to(device)
-                labels = labels.to(device)
-
-                # zero the parameter gradients
-                optimizer.zero_grad()
-
-                # forward
-                # track history if only in train
-                with torch.set_grad_enabled(phase == 'train'):
-                    outputs = model(inputs)
-                    _, preds = torch.max(outputs, 1)
-                    loss = criterion(outputs, labels)
-
-                    # backward + optimize only if in training phase
-                    if phase == 'train':
-                        loss.backward()
-                        optimizer.step()
-
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
-
-            epoch_loss = running_loss / dataset_sizes[phase]
-
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss))
-
-            # deep copy the model
-            if phase == 'val' and epoch_loss < best_loss:
-                best_loss = epoch_loss
-                best_model_wts = copy.deepcopy(model.state_dict())
-
-        print()
-
-    time_elapsed = time.time() - since
-    print('Training complete in {:.0f}m {:.0f}s'.format(
-        time_elapsed // 60, time_elapsed % 60))
-    print('Best Loss: {:4f}'.format(best_loss))
-
-    # load best model weights
-    model.load_state_dict(best_model_wts)
-    return model
+    return average_loss / len(dataloader.dataset)
 
 
-model_ft = models.resnet34(pretrained=True)
-model_ft.fc = nn.Linear(model_ft.fc.in_features, num_classes)
+def test(model, device, dataloader):
+    model.eval()
+    average_loss = 0
 
-model_ft = model_ft.to(device)
+    with torch.no_grad():
+        for i, (inputs, labels) in enumerate(dataloader):
+            if i % 10 == 0:
+                print('Test epoch {} - {}/{}', epoch, i, len(dataloader))
 
-criterion = nn.CrossEntropyLoss()
+            outputs = model(inputs.to(device))
+            loss = criterion(outputs, labels.to(device))
+            average_loss += loss.item() * inputs.size(0)
 
-# Observe that all parameters are being optimized
-# TODO: Try using Adam
-optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+    return average_loss / len(dataloader.dataset)
 
-# Decay LR by a factor of 0.1 every 7 epochs
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
-model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=25)
+if __name__ == '__main__':
+    # Training settingmas
+    parser = argparse.ArgumentParser(description='Model Training Script')
+    parser.add_argument('--batch-size', type=int, default=8, metavar='N', # TODO: Fix bug with batch size
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
+                        help='learning rate (default: 0.001)')
+
+    args = parser.parse_args()
+
+    # TODO: Don't hardcode this
+    num_classes = 11
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+    # Split the full dataset in a training and validation set
+    full_dataset = datasets.ImageFolder('../data/train')
+    train_dataset, val_dataset = train_test_split_dataset(full_dataset, test_size=0.2)
+
+    # Create the data loaders for the training and validation set and employ augmentations for the training data
+    train_loader = data.DataLoader(TransformedDataset(train_dataset, transforms.Compose([
+        transforms.RandomResizedCrop(224, scale=(0.4, 1)),
+        # TODO: Add rotations
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])), batch_size=24, shuffle=True)
+    val_loader = data.DataLoader(TransformedDataset(val_dataset, transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])), batch_size=24, shuffle=False)
+
+    model = models.resnet152(pretrained=True)
+    model.fc = nn.Linear(model.fc.in_features, num_classes)
+    model = model.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+
+    writer = SummaryWriter()
+    epoch = 0
+
+    while True:
+        train_loss = train(model, device, train_loader, optimizer)
+        write_loss(writer, epoch, train_loss, train=True)
+
+        val_loss = test(model, device, train_loader, epoch, writer)
+        write_loss(writer, epoch, val_loss, train=False)
+
+        writer.add_scalar('Learning Rate', optimizer.param_groups[0]['lr'], epoch)
+
+        scheduler.step()
+
+        # TODO: Save best model
